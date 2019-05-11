92b20a7beac472c1a781485dc97f2af0c5b57f3e
Fabian Busch <fabian.busch@onlinehome.de>
2019-05-12 00:09:48 +0200

    changed to condition that all situations have to be found

--- simple_learn_ttt.py
+++ simple_learn_ttt.py
@@ -8,25 +8,26 @@ Created on Mon May  6 21:30:42 2019
 
 from ttt import TicTacToeGame
 from agent import Agent
+import math
 
 agent1 = Agent('Agent1', 0.2, 0.95, 0.3)
 agent2 = Agent('Agent2', 1, 1, 0.8)
 
 game = TicTacToeGame(agent1.decide, agent2.decide)
 
-EPISODES = 100000
+EPISODE = 0
 
-for i in range(EPISODES):
+while len(agent1.getSituations()) < math.pow(3, 9):
+    '''There are 3^9 possible board situations...'''
     done = False
     
     while not done:
         done = game.done()
 
-    if i % 1000 == 0:
-        print('Episode', i, 'of', EPISODES)
+    if EPISODE % 1000 == 0:
+        print('Episode', EPISODE)
         agent1.saveSituations()
         agent2.saveSituations()
 
     game.reset()
-    
-'''There are 3^9 possible board situations...'''
+    EPISODE += 1
