b5e1cb92355ae1b9a7ee9874364ad60a181b608f
Fabian Busch <fabian.busch@onlinehome.de>
2019-05-11 18:46:51 +0200

    added debug support for game class

--- ttt.py
+++ ttt.py
@@ -8,7 +8,8 @@ Created on Sun May  5 22:07:56 2019
 
 class TicTacToeGame:
     """A simple Tic Tac Toe Game"""
-    def __init__(self, cbXStep, cbOStep):
+    def __init__(self, cbXStep, cbOStep, debug=False):
+        self.debug = debug
         self.cbXStep = cbXStep
         self.cbOStep = cbOStep
         self.reset()
@@ -29,9 +30,11 @@ class TicTacToeGame:
         """
 
         if self.done(): # If game is done
-            return resultHandler(0, True)
+            if self.debug: print('game is done')
+            return resultHandler(0, True, None)
 
         if not self.squares[i] == 'N': # In case gameplayer selects illegal field
+            if self.debug: print('illegal field')
             return resultHandler(-1, False, self.__nextStep)
 
         if self.xIsNext:
@@ -44,9 +47,11 @@ class TicTacToeGame:
         self.xIsNext = not self.xIsNext
 
         if self.__winner() == self.squares[i]:
+            if self.debug: print('game is done,', self.squares[i], 'is winner')
             return resultHandler(1, True, None)
 
         if self.done(): # If game is done
+            if self.debug: print('game is done')
             return resultHandler(0, True, None)
 
         return resultHandler(0, False, self.__nextStep)
@@ -55,7 +60,7 @@ class TicTacToeGame:
     def __winner(self):
         winStates = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
         for winState in winStates:
-            if self.squares[winState[0]] and self.squares[winState[1]] == self.squares[winState[0]] and self.squares[winState[2]] == self.squares[winState[0]]:
+            if not self.squares[winState[0]] == 'N' and self.squares[winState[1]] == self.squares[winState[0]] and self.squares[winState[2]] == self.squares[winState[0]]:
                 return self.squares[winState[0]]
         return None
 
@@ -78,6 +83,6 @@ class TicTacToeGame:
         self.squares = []
         self.history = []
         self.xIsNext = True
-        for i in range(0, 9):
-            self.squares[i] = 'N'
+        for i in range(9):
+            self.squares.append('N')
         return self.__nextStep()
