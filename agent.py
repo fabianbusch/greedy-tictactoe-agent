fixed recursive stack overflow

agent.py
@@ -15,6 +15,7 @@ class Agent:
     def __init__(self, name, epsilon, discount, learning_rate, debug=False):
         self.__situations = []
         self.__currentSituationIndex = None
+        self.__currentActionIndex = None
         self.__lastActionIndex = None
         self.debug = debug
         self.epsilon = epsilon
@@ -27,56 +28,78 @@ class Agent:
         if random.uniform(0, 1) < self.epsilon:
             return random.randint(0, 8)
         else:
-            return np.argmax(self.__situations[self.__currentSituationIndex].getActions())
-
-    def chooseAction(self, squares):
-        situation = TTTSituation(squares.copy(), np.zeros([9]))
+            return np.argmax(self.__lastSituation.getActions())
+        
+    def learn(self, reward, done, resultingSquares):
         if self.debug:
-            self.printSituations()
-        sI = 0
-        while sI < len(self.__situations):
-            if self.__situations[sI].compareTo(situation):
-                self.__currentSituationIndex = sI
-                self.__lastActionIndex = self.__get_greedy()
+            print('Handling Result', '\n')
+
+        if reward < 0:
+            self.__lastSituation.setAction(self.__lastActionIndex, reward)
+            return
+
+        if done:
+            self.__lastSituation.setAction(self.__lastActionIndex, reward)
+            return
+
+        resSituation = TTTSituation(resultingSquares.copy(), np.zeros([9]))
+
+        equalSituation = self.__findInSituations(resSituation)
+
+        if equalSituation:
+            return self.calcLastQ(reward, equalSituation)
+        
+        return resSituation
+
+    def calcLastQ(self, reward, currentSituation):
+        if self.__lastActionIndex and self.__lastSituation:
+            next_q = reward + self.discount_factor * \
+                np.max(
+                    currentSituation.getActions())
+            lastActionQ = self.__lastSituation.getAction(
+                self.__lastActionIndex)
+            updateValue = (1-self.learning_rate) * lastActionQ + \
+                self.learning_rate * next_q
+            self.__lastSituation.setAction(
+                self.__lastActionIndex, updateValue)
+
+    def __findInSituations(self, targetSituation):
+        for s in self.__situations:
+            if s.compareTo(targetSituation):
                 if self.debug:
                     print('Found an equal situation:',
-                          self.__situations[sI].getSquares())
-                if self.debug:
-                    print('Choosing Field', self.__lastActionIndex,
-                          'for Board:', situation.getSquares())
-                return self.__lastActionIndex
-            sI = sI + 1
+                          s.getSquares())
+                return s
+        return None
+
+    def decide(self, squares):
+        situation = TTTSituation(squares.copy(), np.zeros([9]))
+
+        if self.debug:
+            self.printSituations()
+
+        equalSituation = self.__findInSituations(situation)
+
+        if equalSituation:
+            self.__lastSituation = equalSituation
+            self.__lastActionIndex = self.__get_greedy()
+            
+            if self.debug:
+                print('Choosing Field', self.__lastActionIndex,
+                        'for Board:', situation.getSquares())
+            return self.__lastActionIndex
+            
         if self.debug:
             print('Saving unkown Situation:', situation.getSquares())
+
         self.__situations.append(situation)
-        self.__currentSituationIndex = len(self.__situations) - 1
+        self.__lastSituation = situation
         self.__lastActionIndex = self.__get_greedy()
+
         if self.debug:
             print('Choosing Field', self.__lastActionIndex,
                   'for Board:', situation.getSquares())
         return self.__lastActionIndex
-
-    def __resultHandler(self, reward, done, readyCallback):
-        if self.debug:
-            print("Handling Result")
-        if self.debug:
-            print('\n')
-        next_q = reward + self.discount_factor * \
-            np.max(
-                self.__situations[self.__currentSituationIndex].getActions())
-        tmpReward = self.__situations[self.__currentSituationIndex].getAction(
-            self.__lastActionIndex)
-        updateValue = (1-self.learning_rate) * tmpReward + \
-            self.learning_rate * next_q
-        self.__situations[self.__currentSituationIndex].setAction(
-            self.__lastActionIndex, updateValue)
-        if not done:
-            readyCallback()
-        return
-
-    def decide(self, squares, stepCallback):
-        stepCallback(self.chooseAction(squares), self.__resultHandler)
-        return
 
     def getSituations(self):
         return self.__situations
@@ -87,6 +110,7 @@ class Agent:
             s.printSituation()
 
     def saveSituations(self, file_path=None):
+        if self.debug: print('Saving situation')
         if not file_path:
             file_path = 'situations_' + self.__name + '.csv'
         formed = []